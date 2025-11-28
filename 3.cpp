6e9facc58225bae570f9c912514c271bce0f6c00
the_hongler <Agari15624@yandex.ru>
2025-11-28 10:43:10 +0300

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -2,9 +2,9 @@
 using namespace std; 
 int main() 
 { 
-    int x=3;
-    cout<< sizeof (int)<<endl;
-    cout<< sizeof (x*10)<<endl;
-    cout<< sizeof (x*0.1)<<endl;
-   return 0; 
+    int x=3; //инициализация переменной
+    cout<< sizeof (int)<<endl; //выводит размер типа int в байтах
+    cout<< sizeof (x*10)<<endl; //выводит размер результата выражения
+    cout<< sizeof (x*0.1)<<endl; //выводит размер результата выражения
+   return 0; //завершение результата 
 }
