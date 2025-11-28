e4e479ef067dbdfce65dff3b8afd1a15821f1516
the_hongler <Agari15624@yandex.ru>
2025-11-28 10:51:08 +0300

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -3,11 +3,11 @@ using namespace std;
  
 int main() 
 { 
-  int x=3, y=0;
-  bool f=false, v=true;
-  cout<<-x<<"\t"<<!x<<endl;
-  cout<<-y<<"\t"<<!y<<endl;
-  cout<<f<<"\t"<<!f<<endl;
-  cout<<v<<"\t"<<!v<<endl;
-  return 0;
+  int x=3, y=0; //объявление двух целых переменных
+  bool f=false, v=true; //объявление двух логических переменных
+  cout<<-x<<"\t"<<!x<<endl; //вывод выражения
+  cout<<-y<<"\t"<<!y<<endl; //вывод выражения
+  cout<<f<<"\t"<<!f<<endl; //вывод выражения(false=0)
+  cout<<v<<"\t"<<!v<<endl; //вывод выражения(true=1)
+  return 0; //завершение программы
 }
