e743e71084101cb7557a890df1ede0c99111a46c
the_hongler <Agari15624@yandex.ru>
2026-04-06 03:36:25 +0300

    Update Individual task 5.cpp

--- Individual task 5.cpp
+++ Individual task 5.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main()
 {
     cout << "y = -x\n";
-    for (int x = -10; x <= 10; x++)
+    for (int x = -10; x <= 10; x++) //цикл x от -10 до 10 с шагом 1
     {
-        cout << "x = " << x << "\t y = " << -x << endl;
+        cout << "x = " << x << "\t y = " << -x << endl; //выводим x и y = -x
     }
     return 0;
 }
