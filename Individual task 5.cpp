c4211b3fdc6f84daaf78c36bfa7921d8d2df502f
the_hongler <Agari15624@yandex.ru>
2026-04-06 10:41:24 +0300

    Individual task 5.cpp

--- Individual task 5.cpp
+++ Individual task 5.cpp
@@ -3,32 +3,24 @@ using namespace std;
 
 int main() 
 {
-    
     double x, y;    // координаты точки
     double r;       // радиус круга
-    
     cout << "Введите радиус круга: ";
     cin >> r;
-    
     cout << "Введите координаты точки (x y): ";
     cin >> x >> y;
-    
     double дист2 = x*x + y*y;
     double r2 = r*r;
-    
     if (дист2 < r2) 
-    {
-        
+    {  
         cout << "Точка лежит внутри" << endl;
     }
     else if (дист2 == r2) 
-    {
-        
+    { 
         cout << "Точка лежит на границе" << endl;
     }
     else 
-    {
-        
+    { 
         cout << "Точка лежит за пределами" << endl;
     }
     return 0;
